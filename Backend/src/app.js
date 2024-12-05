e29fd53e9766c53ab36e4522a44e749c26f4f008
Adrina bernadin <mary.adrina@kalvium.community>
2024-12-05 13:30:42 +0530

    made changes

---
+++ Backend/src/app.js
@@ -0,0 +1,30 @@
+if(process.env.NODE_ENV !=="PRODUCTION")
+    {
+        require('dotenv').config({
+            path:'./src/config/.env',
+        });
+    }
+
+const express = require('express');
+
+const app = express();
+
+app.get("/",(req,res) => {
+    return res.send('Welcome to backend');
+});
+
+module.exports = app;
+
+
+
+
+
+// function express(){
+//     return {
+//         listen:()=>{},
+//         get:()=>{},
+//         listen:()=>{},
+//         listen:()=>{},
+//         listen:()=>{},
+//     }
+// }
