edfde3c0e3e94cf107f52263e9bd3fbd7883f8e8
Adrina bernadin <mary.adrina@kalvium.community>
2024-12-20 20:22:31 +0530

    made changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import Login from './components/login'
-import Signup from './components/signup'
+import LoginPage from './components/auth/login'
+import SignupForm from './components/auth/signup'
 import {Routes, Route} from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
 function App(){
   return (
     <>
-    {/* <Signup/> */}
     <Routes>
-      <Route path="/" />
-      <Route path="/login" element={<Login/>}/>
-      <Route path ="/signup" element={<Signup/>}/>
+        {/* <Route path="/HomePage" element={<h1>Home Page</h1>} /> */}
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       
     </Routes>
     </>
---
+++ Frontend/src/Pages/Signup.jsx
@@ -0,0 +1,6 @@
+import SignupForm from '../components/auth/signup';
+
+export default function Signup() 
+{
+  return <SignupForm />;
+}
