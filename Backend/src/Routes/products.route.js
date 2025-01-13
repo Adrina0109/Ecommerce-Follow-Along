dd7d61bf279785a06f93a79aabc1cc8fe366c3af
Adrina bernadin <mary.adrina@kalvium.community>
2025-01-13 14:57:38 +0530

    milestone14

--- Backend/src/Routes/products.route.js
+++ Backend/src/Routes/products.route.js
@@ -6,6 +6,7 @@ const {
   getProductDataController,
   updateProduct,
   getSingleProductDocument,
+  deleteProduct,
 } = require('../controllers/product.controller.js');
 const router = express.Router();
 
@@ -18,4 +19,5 @@ router.post(
 router.get("/get_product",getProductDataController)
 router.put("/update-products/:id",upload.array("files",5),updateProduct);
 router.get("/get-single/:id", getSingleProductDocument)
+router.delete('/:id', deleteProduct);
 module.exports = router;
