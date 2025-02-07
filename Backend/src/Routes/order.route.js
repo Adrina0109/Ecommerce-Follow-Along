03ac3f55cd005acc0e44d6826cc4fb3d2d68e48b
Adrina bernadin <mary.adrina@kalvium.community>
2025-02-07 17:29:15 +0530

    milestone28

--- Backend/src/Routes/order.route.js
+++ Backend/src/Routes/order.route.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const verifyUser=require('../Middlewares/jwt-verify')
 const router=express.Router()
-const {CreateOrder,GetuserOrders}=require('../controllers/order.controller')
+const {CreateOrder,GetuserOrders,CancelOrder}=require('../controllers/order.controller')
 router.post('/confirm-order',verifyUser,CreateOrder)
 router.get('/user-orders-data',verifyUser ,GetuserOrders)
+router.patch('/cancel-order',verifyUser,CancelOrder);
 module.exports=router
--- Frontend/src/Pages/OrderHistory.jsx
+++ Frontend/src/Pages/OrderHistory.jsx
@@ -1,42 +1,59 @@
-import {useEffect, useState} from 'react';
-import axios from 'axios'
-import CartCard from "../components/ProductCard/CartCard";
-function OrderHistory(){
-    const [Order, SetOrderData]= useState([])
-    const fetchedOrderedProducts= async()=>{
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import CartCard from '../components/ProductCard/CartCard';
+
+function OrderHistory() {
+  const [OrderedData, SetOrderedData] = useState([]);
+  const fetchedOrderedProducts = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return alert('Token is missing , Please login');
+    }
+    const response = await axios.get(
+      `http://localhost:8080/orders/user-orders-data?token=${token}`
+    );
+    const reversedData = response.data.orders?.reverse();
+
+    console.log(response.data.orders);
+    SetOrderedData(reversedData);
+  };
+  useEffect(() => {
+    fetchedOrderedProducts();
+  }, []);
+  const handleCancel = async (id) => {
+    console.log(id);
     const token = localStorage.getItem('token');
     if (!token) {
       return alert('Token is missing,Please login');
     }
-        const response = await axios.get(
-            `http://localhost:8080/orders/user-orders-data?token=${token}`
-        )
-        SetOrderData(response.data.orders);
-        console.log(response.data.data)
-    }
-    useEffect(()=>{
-        fetchedOrderedProducts()
-    },[]);
-    return (
-        <div>
-          ordered
-          {Order?.map((singleCartObject, index) => {
-            return (
-              <div key={index}>
-                <CartCard
-                  title={singleCartObject.orderItems.title}
-                  images={singleCartObject.orderItems.images[0]}
-                  description={singleCartObject.orderItems.description}
-                  originalPrice={singleCartObject.orderItems.originalPrice}
-                  discountedPrice={singleCartObject.orderItems.discountedPrice}
-                  id={singleCartObject.orderItems._id}
-                  orderStatus={singleCartObject.orderStatus}
-                  createdBy={'adrina@gmail.com'}
-                />
-              </div>
-            );
-          })}
-        </div>
-      );
+    await axios.patch(
+      `http://localhost:8080/orders/cancel-order?token=${token}&orderId=${id}`
+    );
+    fetchedOrderedProducts();
+  };
+  return (
+    <div>
+      ordered
+      {OrderedData?.map((singleCartObject, index) => {
+        return (
+          <div key={index}>
+            <CartCard
+              title={singleCartObject.orderItems.title}
+              images={singleCartObject.orderItems.images[0]}
+              //   Index={index}
+              description={singleCartObject.orderItems.description}
+              originalPrice={singleCartObject.orderItems.originalPrice}
+              discountedPrice={singleCartObject.orderItems.discountedPrice}
+              id={singleCartObject._id}
+              orderStatus={singleCartObject.orderStatus}
+              createdBy={'adrina@gmail.com'}
+              handleCancel={handleCancel}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+
 export default OrderHistory;
